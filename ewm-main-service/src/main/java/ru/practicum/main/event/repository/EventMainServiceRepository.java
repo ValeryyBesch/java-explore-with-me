ce072a07d62aafde3cbab9275e562f7fe548b45b
Valera <valerapro.66@yandex.ru>
2024-05-24 15:45:13 +0300

    create: main_svc

--- ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventMainServiceRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventMainServiceRepository.java
@@ -17,37 +17,41 @@ public interface EventMainServiceRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByIdAndInitiatorId(long eventId, long userId);
 
     @Query("SELECT e " +
-            "FROM Event e " +
-            "WHERE (e.initiator.id IN :userIds OR :userIds IS null) " +
-            "AND (e.state IN :states OR :states IS null) " +
-            "AND (e.category.id IN :categories OR :categories IS null) " +
-            "AND (e.eventDate > :start OR :start IS null) " +
-            "AND (e.eventDate < :end OR :end IS null) ")
-    List<Event> findAllByParam(@Param("userIds") List<Long> userIds,
-                               @Param("states") List<State> states,
-                               @Param("categories") List<Long> categories,
-                               @Param("start") LocalDateTime start,
-                               @Param("end") LocalDateTime end,
-                               Pageable pageable);
+            "FROM ru.practicum.main.event.model.Event e " +
+            "WHERE (e.initiator.id IN ?1 OR ?1 IS null) " +
+            "AND (e.state IN ?2 OR ?2 IS null) " +
+            "AND (e.category.id IN ?3 OR ?3 IS null) " +
+            "AND (e.eventDate > ?4 OR ?4 IS null) " +
+            "AND (e.eventDate < ?5 OR ?5 IS null) ")
+    List<Event> findAllByParam(List<Long> users, List<State> states, List<Long> categories,
+                               LocalDateTime start, LocalDateTime end, Pageable pageable);
 
     boolean existsByIdAndInitiatorId(long eventId, long userId);
 
     @Query("SELECT e " +
             "FROM Event e " +
-            "WHERE ((:text IS null) OR ((lower(e.annotation) LIKE concat('%', lower(:text), '%')) OR (lower(e.description) LIKE concat('%', lower(:text), '%')))) " +
-            "AND (e.category.id IN :categories OR :categories IS null) " +
-            "AND (e.paid = :paid OR :paid IS null) " +
-            "AND (e.eventDate > :rangeStart OR :rangeStart IS null) AND (e.eventDate < :rangeEnd OR :rangeEnd IS null) " +
-            "AND (:onlyAvailable = false OR ((:onlyAvailable = true AND e.participantLimit > (SELECT count(*) FROM Request AS r WHERE e.id = r.event.id))) " +
-            "OR (e.participantLimit > 0 )) ")
-    List<Event> findAllEvents(@Param("text") String text,
-                              @Param("categories") List<Long> categories,
-                              @Param("paid") Boolean paid,
-                              @Param("rangeStart") LocalDateTime rangeStart,
-                              @Param("rangeEnd") LocalDateTime rangeEnd,
-                              @Param("onlyAvailable") Boolean onlyAvailable,
-                              String sort,
-                              Pageable pageable);
+            "WHERE " +
+            "(:text IS null OR (lower(e.annotation) LIKE concat('%', lower(:text), '%')) OR (lower(e.description) LIKE concat('%', lower(:text), '%'))) " +
+            "AND (:categories IS null OR e.category.id IN :categories) " +
+            "AND (:paid IS null OR e.paid = :paid) " +
+            "AND (:rangeStart IS null OR e.eventDate > :rangeStart) " +
+            "AND (:rangeEnd IS null OR e.eventDate < :rangeEnd) " +
+            "AND (:onlyAvailable = false OR (:onlyAvailable = true AND (e.participantLimit > (SELECT count(r) FROM Request r WHERE r.event.id = e.id))) OR (e.participantLimit > 0)) " +
+            "ORDER BY " +
+            "CASE " +
+            "WHEN :sort = 'title' THEN e.title " +
+            "WHEN :sort = 'date' THEN e.eventDate " +
+            "ELSE e.id " +
+            "END")
+    List<Event> findAllEvents(
+            @Param("text") String text,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            @Param("onlyAvailable") Boolean onlyAvailable,
+            @Param("sort") String sort,
+            Pageable pageable);
 
     boolean existsByCategoryId(long catId);
 }
