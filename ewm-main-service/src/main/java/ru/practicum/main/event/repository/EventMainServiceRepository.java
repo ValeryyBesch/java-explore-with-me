d4d8516b186f5105b746fa91d94268d7995c6d7a
Valera <valerapro.66@yandex.ru>
2024-05-24 13:35:01 +0300

    create: main_svc

--- ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventMainServiceRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventMainServiceRepository.java
@@ -13,30 +13,31 @@ import java.util.Optional;
 public interface EventMainServiceRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByInitiatorId(long userId, Pageable pageable);
 
-
     Optional<Event> findByIdAndInitiatorId(long eventId, long userId);
 
     @Query("SELECT e " +
             "FROM Event e " +
-            "WHERE (e.initiator.id IN ?1 OR ?1 IS null) " +
-            "AND (e.state IN ?2 OR ?2 IS null) " +
-            "AND (e.category.id IN ?3 OR ?3 IS null) " +
-            "AND (e.eventDate > ?4 OR ?4 IS null) " +
-            "AND (e.eventDate < ?5 OR ?5 IS null) ")
-
+            "WHERE (e.initiator.id IN :users OR :users IS null) " +
+            "AND (e.state IN :states OR :states IS null) " +
+            "AND (e.category.id IN :categories OR :categories IS null) " +
+            "AND (e.eventDate > :start OR :start IS null) " +
+            "AND (e.eventDate < :end OR :end IS null) ")
     List<Event> findAllByParam(List<Long> users, List<State> states, List<Long> categories,
                                LocalDateTime start, LocalDateTime end, Pageable pageable);
 
     boolean existsByIdAndInitiatorId(long eventId, long userId);
 
-@Query("SELECT e " +
-        "FROM Event e " +
-        "WHERE ((?1 IS null) OR ((lower(e.annotation) LIKE concat('%', lower(?1), '%')) OR (lower(e.description) LIKE concat('%', lower(?1), '%')))) " +
-        "AND (e.category.id IN ?2 OR ?2 IS null) " +
-        "AND (e.paid = ?3 OR ?3 IS null) " +
-        "AND (e.eventDate > ?4 OR ?4 IS null) AND (e.eventDate < ?5 OR ?5 IS null) " +
-        "AND (?6 = false OR ((?6 = true AND e.participantLimit > (SELECT count(*) FROM Request AS r WHERE e.id = r.event.id))) " +
-        "OR (e.participantLimit > 0 )) ")
+    @Query("SELECT e " +
+            "FROM Event e " +
+            "LEFT JOIN Request r ON e.id = r.event.id " +
+            "WHERE ((:text IS null) OR ((lower(e.annotation) LIKE concat('%', lower(:text), '%')) OR (lower(e.description) LIKE concat('%', lower(:text), '%')))) " +
+            "AND (e.category.id IN :categories OR :categories IS null) " +
+            "AND (e.paid = :paid OR :paid IS null) " +
+            "AND (e.eventDate > :rangeStart OR :rangeStart IS null) " +
+            "AND (e.eventDate < :rangeEnd OR :rangeEnd IS null) " +
+            "AND (:onlyAvailable = false OR ((:onlyAvailable = true AND e.participantLimit > COUNT(r.id))) " +
+            "OR (e.participantLimit > 0)) " +
+            "GROUP BY e.id")
     List<Event> findAllEvents(String text, List<Long> categories, Boolean paid, LocalDateTime rangeStart,
                               LocalDateTime rangeEnd, Boolean onlyAvailable, String sort, Pageable pageable);
 
